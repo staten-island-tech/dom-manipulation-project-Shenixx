a80c3fee1c3ecaa47e6b2ffff5934365d3f496dc
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-10-17 10:14:21 -0400

    asdasda

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const DOMSelectors = {
-    form: document.querySelector("#form"),
-    firstName:document.querySelector(".first-name"),
-    h2s:document.querySelectorAll("h2"), 
-};
+// const DOMSelectors = {
+//     form: document.querySelector("#form"),
+//     firstName:document.querySelector(".first-name"),
+//     h2s:document.querySelectorAll("h2"), 
+// };
 
-DOMSelectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
+// DOMSelectors.form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     console.log(DOMSelectors.firstName.value);
     
-    DOMSelectors.h2s.forEach(
-        (el) =>(el.textContent = DOMSelectors.firstName.value)
-    );
-});
+//     DOMSelectors.h2s.forEach(
+//         (el) =>(el.textContent = DOMSelectors.firstName.value)
+//     );
+// });
+
+const button = document.getElementById("btn");
+console.log(button);
