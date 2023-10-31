525f32f3e2d9c3b3fad213de41ce261b67dded1f
Shenixx <113193025+Shenixx@users.noreply.github.com>
2023-10-31 10:15:33 -0400

    asdsadas

--- app.js
+++ app.js
@@ -9,26 +9,29 @@ const DOMSelectors = {
 };  
 
 function makeAlbum(){
+    
     DOMSelectors.button.addEventListener("click", function () {
-        const name = nameInput.value;
-        const age = ageInput.value;
-        const infoStuff = {
+        const name = DOMSelectors.nameInput.value;
+        const age = DOMSelectors.ageInput.value;
+        return {
             name: name,
             age: age,
-        };
-        return infoStuff;
+          };
     })
-}
-
-// function newCard() {
-//         DOMSelectors.box.insertAdjacentHTML("afterend", `<div class=newInfo> <h2 class=> My name is ${name} and my age is ${age} </h2>  
-//         <button type="removeButton" id="removeButton">remove</removeButton> </div>`);
-//         DOMSelectors.nameInput.value = "";
-//         DOMSelectors.ageInput.value = "";
-// };
+};
 
 function newCard() {
-    DOMSelectors.box.insertAdjacentHTML("afterend", `<div class=newInfo> <h2 class=> My name is ${name} and my age is ${age} </h2>`);}
+    DOMSelectors.button.addEventListener("click", function () {
+        const data = makeAlbum();
+        DOMSelectors.box.insertAdjacentHTML("afterend", `<div class=newInfo> <h2 class=> My name is ${data.name} and my age is ${data.age} </h2>  
+        <button type="removeButton" id="removeButton">remove</removeButton> </div>`);
+        DOMSelectors.nameInput.value = "";
+        DOMSelectors.ageInput.value = "";
+    })
+};
+
+// function newCard() {
+//     DOMSelectors.box.insertAdjacentHTML("afterend", `<div class=newInfo> <h2 class=> My name is ${} and my age is $} </h2>`);}
 
 // function removeButton(){
 //     let btns = document.querySelectorAll("newInfo")
@@ -39,7 +42,7 @@ function newCard() {
 // }
 
 
-makeAlbum()
+makeAlbum();
 newCard()
 
 
